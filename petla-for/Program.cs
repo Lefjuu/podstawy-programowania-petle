2b824a2781c56c2657a46fec3f0d32449b0eba82
unknown <dawid.nozownik@gmail.com>
2023-12-09 11:47:54 +0100

    petla-for

---
+++ petla-for/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        for (int x = 0; x <= 10; x++)
+        {
+            double y = Math.Sqrt(Math.Pow(3, 2)) * x;
+            Console.WriteLine($"Dla x = {x}, y = {y}");
+        }
+
+        Console.WriteLine("\nNaciśnij dowolny klawisz, aby zakończyć...");
+        Console.ReadKey();
+    }
+}
