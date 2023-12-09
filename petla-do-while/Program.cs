15408f42fb39dc8018dc51b7ac6980941aa1e7fd
unknown <dawid.nozownik@gmail.com>
2023-12-09 12:01:58 +0100

    petla-do-while

---
+++ petla-do-while/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace petlaDoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int liczba = 1;
+
+            do
+            {
+                int wynik = liczba * liczba;
+                Console.WriteLine($"{liczba} do kwadratu = {wynik}");
+                liczba++;
+            } while (liczba <= 20);
+
+            Console.ReadKey();
+        }
+    }
+}
