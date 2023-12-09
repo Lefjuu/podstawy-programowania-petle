3f6763ed2034fc05e2f59dab5a609886067cb5aa
unknown <dawid.nozownik@gmail.com>
2023-12-09 12:06:17 +0100

    petla foreach

---
+++ for-each/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class petlaForEach
+{
+    static void Main()
+    {
+        string[] pets = { "cat", "dog", "parrot", "guinea pig" };
+
+        foreach (string pet in pets)
+        {
+            if (pet == "dog")
+            {
+                Console.WriteLine("Skipping 'dog'");
+                continue;
+            }
+            Console.WriteLine(pet);
+        }
+
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+    }
+}
